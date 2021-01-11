5d682f842cb0962552fa416472798c7cc1d0bd01
Amyavow <amy.avow@gmail.com>
2021-01-11 19:01:17 +0100

    include ordinal Scale challenge

--- D3Scales/Lab3.js
+++ D3Scales/Lab3.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-undef */
 
 /* Example 1: Drawing a Line Graph using D3 Linera Scale
@@ -77,7 +78,7 @@ d3.json('../data/monthlySales.json')
         d3.min(data, (d) => d.month),
         d3.max(data, (d) => d.month),
       ])
-      .range([0, lineWidth])
+      .range([padding, (lineWidth - padding)])
       .nice();
 
     // define y Linear Scale
@@ -86,7 +87,7 @@ d3.json('../data/monthlySales.json')
         d3.min(data, (d) => d.sales),
         d3.max(data, (d) => d.sales),
       ])
-      .range([lineHeight, 10])
+      .range([(lineHeight - padding), 10])
       .nice();
 
     // Define x and Y axis
@@ -141,7 +142,68 @@ d3.json('../data/monthlySales.json')
 */
 d3.json('../data/viewership.json');
 
-/* TO DO 3: Stretch Challenge
+/* Example 3: Drawing a bar Chart Using Ordinal Scale
+  We create a color scale using scaleOrdinal
+  the Scale maps the years in '..data/eventDates.json' to colors.
+ */
+
+d3.json('../data/eventDates.json')
+  .then((data) => {
+    const svg = d3.select('svg#example3');
+
+    const parseTime = d3.timeParse('%B %d, %Y');
+
+    const dates = [];
+    for (const obj of data) {
+      dates.push(parseTime(obj.date));
+    }
+
+    let domain = d3.extent(dates);
+    domain = [d3.timeYear.floor(dates[0]), d3.timeYear.ceil(domain[1])];
+
+    const xScale = d3.scaleTime()
+      .domain(domain)
+      .range([25, 555]);
+
+    const xAxis = d3.axisBottom(xScale)
+      .ticks(d3.timeYear);
+
+    svg.append('g')
+      .attr('transform', 'translate(0,70)')
+      .call(xAxis);
+
+    const years = d3.map(dates, (d) => d.getFullYear()).keys();
+
+    const colorScale = d3.scaleOrdinal()
+      .domain(years)
+      .range(['#000000', '#A4A4A4', '#1DA1F2', '#810081']);
+
+    svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', (d) => xScale(parseTime(d.date)))
+      .attr('y', 40)
+      .attr('width', 10)
+      .attr('height', 30)
+      .attr('fill', (d) => colorScale(parseTime(d.date).getFullYear()));
+  });
+
+/*
+  TO DO 3:
+  Create a bar grapgh with the data in '..distanceCovered.json'
+  x axis = date
+  y axis = distance covered
+
+  HINT: Use ScaleLinear to scale values on y axis
+    Use ScaleOrdinal to scale values on the x axis
+*/
+d3.json('../data/distanceCovered.json')
+  .then(() => {
+
+  });
+
+/* TO DO 4: Stretch Challenge
   Create a Bar Chart using data in '..data/viewrship.josn'
   x axis = Episode Number
   y axis = US Viewership
@@ -149,10 +211,11 @@ d3.json('../data/viewership.json');
 
 d3.json('../data/viewership.json');
 
-/* TO DO 4: Stretch Challenge
+/* TODO 5: Stretch Challenge
   Draw a Scatter Plot from the city population data in '..data/cities.csv'
   x axis = city label
   y axis = population
+  Make sure to use the d3.scaleLinear function to scale the population numbers
 */
 
 d3.csv('../data/cities.csv');
